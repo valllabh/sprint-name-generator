b8720db3cb13e66a043ade63df659eb613416e20
vallabh <vallabh.joshi@gmail.com>
2023-11-23 09:09:50 +0000

    Added formatting and URL input support

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ function generateSprintName() {
 
     var sprintStartDate = moment($("#sprint-start-date").val());
     var sprintEndDate = moment($("#sprint-end-date").val());
-    var sprintDurationWeeks = moment.duration($("#sprint-duration").val(), 'weeks');
+    var sprintDurationWeeks = moment.duration($("#sprint-duration").val(), 'weeks').subtract(1, 'day');
 
     if (sprintPrefix && sprintStartDate && sprintEndDate && sprintStartDate.isBefore(sprintEndDate) && sprintDurationWeeks) {
 
@@ -59,7 +59,70 @@ function generateSprintName() {
     return false
 }
 
+function writeSampleURL() {
+    $("#sample-url").text(window.location.origin + window.location.pathname + "?prefix=" + $("#sprint-prefix").val() + "&number=" + $("#sprint-number").val() + "&duration=" + $("#sprint-duration").val());
+}
+
+// On page load
 $(document).ready(function () {
-    // set start date form element to today's date
-    $("#sprint-start-date").val(moment().format("YYYY-MM-DD"));
+
+    // read url params and set sprint prefix and sprint number
+    var urlParams = new URLSearchParams(window.location.search);
+    var sprintPrefix = urlParams.get('prefix');
+    var sprintNumber = urlParams.get('number');
+    var sprintDuration = urlParams.get('duration');
+    var sprintStartDate = moment(urlParams.get('start'));
+    var sprintEndDate = moment(urlParams.get('end'));
+
+    // if sprintStartDate is not valid then set to today
+    if (!sprintStartDate.isValid()) {
+        sprintStartDate = moment().startOf('day');
+    }
+
+    // if sprintEndDate is not valid then set to today + 8 weeks
+    if (!sprintEndDate.isValid()) {
+        sprintEndDate = moment().startOf('day').add(8, 'weeks');
+    }
+
+    // sprintPrefix validate if not null and then set
+    if (sprintPrefix) {
+        $("#sprint-prefix").val(sprintPrefix);
+    } else {
+        $("#sprint-prefix").val("SPRINT");
+    }
+
+    // sprintNumber validate if number grater than 0 and then set
+    if (sprintNumber && parseInt(sprintNumber) > 0) {
+        $("#sprint-number").val(sprintNumber);
+    } else {
+        $("#sprint-number").val(1);
+    }
+
+    // sprintDuration validate if number grater than 0 and then set
+    if (sprintDuration && parseInt(sprintDuration) > 0) {
+        $("#sprint-duration").val(sprintDuration);
+    } else {
+        $("#sprint-duration").val(2);
+    }
+
+
+    // sprintStartDate validate if valid date and then set
+    if (sprintStartDate.isValid()) {
+        $("#sprint-start-date").val(sprintStartDate.format('YYYY-MM-DD'));
+    }
+
+    // sprintEndDate validate if valid date and then set
+    if (sprintEndDate.isValid() && sprintEndDate.isAfter(sprintStartDate)) {
+        $("#sprint-end-date").val(sprintEndDate.format('YYYY-MM-DD'));
+    }
+
+    writeSampleURL();
+
+    // generate sprint name
+    generateSprintName();
+
+    // on form input change using jquery call function
+    $(".sprint-config :input").change(writeSampleURL);
+
 })
+
